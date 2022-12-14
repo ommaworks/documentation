f4c2d2110369d9b441c699b42da8c309a0ad6c8e
Selman OZTURK <selman@ommaworks.com>
2022-12-14 16:31:02 +0300

    :sparkles: Add empty Android project [VQ-583]

---
+++ VQ/implementation-methods/data-injection-on-native-apps/android-example/app/src/main/java/com/ommasign/vq_messaging/MainActivity.java
@@ -0,0 +1,14 @@
+package com.ommasign.vq_messaging;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
